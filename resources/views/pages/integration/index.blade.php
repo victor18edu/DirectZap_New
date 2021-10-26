92adf51a058346598835c3437a78a0f4695da481
Victor Eduardo <39489857+victor18edu@users.noreply.github.com>
2021-10-26 16:21:12 -0300

    conection braip

--- resources/views/pages/integration/index.blade.php
+++ resources/views/pages/integration/index.blade.php
@@ -5,7 +5,8 @@
         <div class="row">
             <div class="col-md-5">
                 <label>Token</label>
-                <input type="text" name="token" id="" class="form-control" value="df351daaec0b5f2dc849f240f8fdbda3f608dcce">
+                <input type="text" name="token" id="" class="form-control"
+                    value="df351daaec0b5f2dc849f240f8fdbda3f608dcce">
             </div>
         </div>
         <input type="button" onclick="braip()" value="braip">
@@ -28,14 +29,14 @@
         }
 
         function braip() {
-            var token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6Ijc5YjhlNGQ5MTU4MWM3YWNiYTM4Y2JlZGQ2ZDU5YTk3ZmE1MGU4ZDBmMjllOTM0Mzg4YmRhZjA3NzE4OWIxZDFlMGMxYWIwODdkZDVmNTA4In0.eyJhdWQiOiI0MjU4NSIsImp0aSI6Ijc5YjhlNGQ5MTU4MWM3YWNiYTM4Y2JlZGQ2ZDU5YTk3ZmE1MGU4ZDBmMjllOTM0Mzg4YmRhZjA3NzE4OWIxZDFlMGMxYWIwODdkZDVmNTA4IiwiaWF0IjoxNjI4ODExMDQwLCJuYmYiOjE2Mjg4MTEwNDAsImV4cCI6MTY2MDM0NzA0MCwic3ViIjoiMzM4OTQ0MiIsInNjb3BlcyI6W119.muNthMtepg5rfRKg2J9XGbcUErqPRf6Qwg3DLoJcjbzWFgj7Cmke6LvQgfm1FEUTZQ7n6u9mZBPN7JqojcN7lURyTg0lPiFCTrkqL5MOI5AFiGt4NIMA4cpUCN6fbV6UL6ZAyHZSXxBlJHPkuMNTzINxK6RVGhGLmB8qvIv9gSzY7WgLz_2eddyrCIxQwPYOPQhAuBWTHRBwV570pMkujvtnBmwmm9r7a-FmQATSdoQqxK4ZUjJpX7AVZzNdn41WcWv9sddDCF5JtzJRLhUPNNQE8JbxgDT-ccRPLDfXRNG8772wbFzkokxn4s08SUaVBUnwJ9x2F4xJSRP9hBF3dWJZ1Hg30isCHctDSvBpahzAvXl7bMgJUND6fJU3_hrjBImVH11fXQHzGMO8f9tdlpK2reAx_7JcbVM01N5jpuQj2MgIKo3UNE1jJLZ8114efx9OS2lV_oJuHVMnRSimC_D5vUJL6HCBVnwn5vFTC42caR08Cq615rseTmpSleXIYToszGHMdUzC6FLg6W_O3Lt_SNuxaIScWQEJvEg_3_qE-ADszWH_KEKLCLJPG4m8Uczwpl0XC2rU12lpaG84BeA_wnwNFlWAbTtfu60JiV5ZJ3IBQ18PKFAL2PYIOVk2mnJlCtN968C874yywER0dFg9LG7xfUUdSeekdQS7aBg";
+            var token ="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjE1YmM4NjdjZWMwYjQzMmI0YzA1ZDNmNjBiZDk2NGFmZmQ1YzA3YjBlNDE1N2ZkMjIzYmE3MTBmNDM3YzcxMGYwYjFmZDE0NjRmNDFmYzljIn0.eyJhdWQiOiI0NjI3MyIsImp0aSI6IjE1YmM4NjdjZWMwYjQzMmI0YzA1ZDNmNjBiZDk2NGFmZmQ1YzA3YjBlNDE1N2ZkMjIzYmE3MTBmNDM3YzcxMGYwYjFmZDE0NjRmNDFmYzljIiwiaWF0IjoxNjM1MjcyNTg3LCJuYmYiOjE2MzUyNzI1ODcsImV4cCI6MTY2NjgwODU4Nywic3ViIjoiMzM4OTQ0MiIsInNjb3BlcyI6W119.SMVyxbINeWtV9-AWvCloGGn74sBS4z-HmtpTa2k_Sqf9epyZ2u6qkTWjWhCIyF1ZS4ia-RpuEinpmFs9J3uRgp1C-HmuvH_JWTiFjIHNupWCKaJtq35Z8JWjE-4Q2rGtuXnxArdzG4f80r2Dnuadp-rFQbxVMfVgVWuay2uXBtse0WGsaPK8zrj855i8VP1kAN30NNgm1Bl9AHIUfNDjWibCD28oSqr1HxV9JKSrPFdNMEg5XXOvoH9H1B51urA93A-slSkCMC9vYU1PMTIQ8LqQxsVLWa9eI7iBNHvmZCBVPH7Xg_qRv3s-3E5ABz9H4u5LUOGxo3-M-lWYeFZOEv6Rq7vHTbmITBW8KI4oAoG2Rb2SEim-32sVmJ8kEQCyakAc35qG9jurqQXBG9f3p2vdd9Bceuz0jfJF-KGiSnROkWxNFbmB1YZU-fPtyiuA8JY2W6pbGj6xB4YDBwYs4f-QQgnIe5ECoHBgJuTkJxmuCWwWq2FM8JfQ3RQManDM41Lv1yoEBsqn6z9kWpC7Tvs1k5l3GHA40F6pCVoTMB26E9g4sY9nLx6ggIGh042EgUw2EOOc9SnZ_epeoKtpxKCfP5mp-jz7YcUoeqYc31GvjBxsf32gkJJr89iY8nldvZNbf_4acxuJU6Hz3NYJzCYEXvXNqgCW90IbOEM1xv4";
             $.ajax({
                 type: "GET",
                 dataType: "json",
-                data: {
-                    "authorization": token,
+                headers: {
+                    'Authorization': token,
                 },
-                url: 'https://ev.braip.com/api/vendas',
+                url: 'https://ev.braip.com/api/v1/producers/products',
                 success: function(response) {
                     console.log(response);;
                 }
